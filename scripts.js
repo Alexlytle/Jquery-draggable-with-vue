4a2ab8be70f7ac5bc4b5946d94dbc7aa29b3d184
Alexlytle <alex.joe.lytle@gmail.com>
2022-08-21 13:09:44 -0700

    added editing
    added editing

--- scripts.js
+++ scripts.js
@@ -8,19 +8,19 @@
         drag:['drag one','drag two'],
         items:['item 1','item 2','item 3','item 4','item 5'],
         dragging:null,
-        currentIndex:null
+        currentIndex:null,
+        name:'',
+        editingTrue:false
       }
     },
     mounted(){
             $( "#sortable" ).sortable({
               revert: true,
               update:(event, ui )=>{
-                // console.log(ui.item.index(),ui.item[0].innerHTML)
                 if(this.dragging){
                     this.updateArray(ui)
                     this.dragging = false
                 }else{
-                    
                     let itemArray = []
                     let myPromise = new Promise((myResolve, myReject)=>{
                                 myResolve(); // when successful
@@ -28,24 +28,26 @@
                         });
                         myPromise.then(()=> { 
                                     this.items.length = 0;
-                                    document.querySelectorAll('.item').forEach(element => itemArray.push(element.innerText));
+                                    
+                                    document.querySelectorAll('.item').forEach(element => {
+                                        itemArray.push(element.querySelector('p').innerText)
+  
+                                    });
                                 },
                         ).then(()=>{
                                     this.items = itemArray
                                 },    
                         )
                     }
-
             } 
             });
             $( ".draggable" ).draggable({
-              connectToSortable: "#sortable",
-              helper: "clone",
-              revert: "invalid",
-              start :()=>{
-                this.dragging = true
-              }
-             
+                    connectToSortable: "#sortable",
+                    helper: "clone",
+                    revert: "invalid",
+                    start :()=>{
+                        this.dragging = true
+                    }
             });
             $( "ul, li" ).disableSelection();
  
@@ -54,6 +56,19 @@
         updateArray(ui){
             this.items.splice(ui.item.index(),0, ui.item[0].innerHTML)
             ui.item.remove()
+        },
+        updateText(index){
+            this.currentIndex = index
+            this.editingTrue = true
+            this.name = this.items[index]
+   
+          
+        },
+        submit(){
+         
+                this.items.splice(this.currentIndex,1,this.name)
+                this.editingTrue = false
         }
+
     }
   }).mount('#app')
